472ef6976464027c6f5e8dec5810410b36515639
bakzh <109787790+bakzh@users.noreply.github.com>
2022-07-25 07:54:05 +0900

    Delete day10 directory

--- day10/src/com/kh/exam1/PersonMain.java
+++
@@ -1,8 +0,0 @@
-package com.kh.exam1;
-
-public class PersonMain {
-    public static void main(String[] args) {
-        Person p1 = new Person("홍길동");
-        Person p2 = new Person("홍길순", 20);
-    }
-}
--- day10/src/com/kh/exam6/Main.java
+++
@@ -1,7 +0,0 @@
-package com.kh.exam6;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
