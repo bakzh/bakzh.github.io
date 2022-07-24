8b9e813b0401781a8d483af78194d9f23098ced7
bakzh <109787790+bakzh@users.noreply.github.com>
2022-07-25 07:55:10 +0900

    Delete day5 directory

--- day5/src/StringTest.java
+++
@@ -1,9 +0,0 @@
-public class StringTest {
-
-    public static void main(String[] args) {
-
-        String name = new String("홍길동");
-        char ch = name.charAt(2);
-        System.out.println("ch = " + ch);
-    }
-}
